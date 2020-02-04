33ce355543b552a0aaf2626c3f7a19abd16cfa2e
Misha Bukhalo <bukhalo1999@gmai.com>
2020-02-04 16:51:01 +0200

    Initial commit

---
+++ Laboratory work 1/Laboratory work 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
